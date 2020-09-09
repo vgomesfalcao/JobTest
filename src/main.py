89af35158aa2ed174fbd1760f41d5b7ae36d219b
Vinicius Gomes <vgomesfalcao@gmail.com>
2020-09-09 12:10:18 -0300

    add main.py and fibonacci function

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#x = input('Input the value of x:')
+#y = input('Input the value of y:')
+#n = input('Input the value of n:')
+
+fibo = []
+
+
+def fibonacci(value:int, lis:list):
+    if lis == None:
+        lis.append()
