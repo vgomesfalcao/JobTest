57b07235bbe6f2f66146950897ec1528cae84510
Vinicius Gomes <vgomesfalcao@gmail.com>
2020-09-09 12:34:40 -0300

    import fibo file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
+from fibo import fibonacci,init_fibo
+
 #x = input('Input the value of x:')
 #y = input('Input the value of y:')
 #n = input('Input the value of n:')
 
-fibo = []
+
+n = 8
 
 
-def fibonacci(value:int, lis:list):
-    if lis == None:
-        lis.append()
+print(init_fibo(n))
